83e951faaf12c7c2b84601cd7ad306249a7fc8e9
Oscar Linger <oscarlinger@gmail.com>
2017-03-24 17:05:28 +0100

    enhanced structure and new function

--- app/js/musicService.js
+++ app/js/musicService.js
@@ -2,6 +2,7 @@ musicQuizApp.factory('Quiz',
   ['$cookies', '$resource', function ($cookies, $resource) {
 
   var numberOfQuestions = 0;
+  var numberOfCorrectAnswers = 0;
 
   var quizSongs = [];
 
@@ -10,28 +11,39 @@ musicQuizApp.factory('Quiz',
     $cookies.put('numberOfQuestions', number);
   }
 
+  // Get the number of questions in the quiz
   this.getNumberOfQuestions = function() {
     return parseInt($cookies.get('numberOfQuestions'));
+  }
+
+  // Set the number of correctly answered questions in the quiz
+  this.setNumberOfCorrectAnswers = function(number) {
+    $cookies.put('numberOfCorrectAnswers', number);
+  }
+
+  // Get the number of correctly answered questions in the quiz
+  this.getNumberOfCorrectAnswers = function() {
+    return parseInt($cookies.get('numberOfCorrectAnswers'));
+  }
+
+  // Get the list of chosen music (UNFINISHED)
+  this.getChosenQuizMusic = function() {
     }
 
-  this.getQuizList = function() {
-    }
+  // Add new music e.g. album or artist (UNFINISHED)
+  this.addMusicToQuiz = function(music) {
+    $cookies.put(music.id, music.id);
+  }
 
+  // Remove music from quizMusicList e.g. album or artist (UNFINISHED)
+  this.removeMusicFromQuiz = function(musicID) {
+    $cookies.remove(musicID);
+  }
 
-this.addMusicToQuiz = function(music) {
-   $cookies.put(music.id, music.id);
-}
-
-this.removeMusicFromQuiz = function(musicID) {
-  $cookies.remove(musicID);
-}
-
-
+  // API request (UNFINISHED)
   this.MusicSearch = $resource('https://api.spotify.com',{},{
 
   });
 
-
   return this;
-
 }]);
