763ccd646d9e3a89eae156636d91baca93f2f878
Mai-Khanh LE <lemaikhanh.lmk@gmail.com>
2017-03-30 13:15:12 +0200

    add sidebar

--- app/js/musicService.js
+++ app/js/musicService.js
@@ -13,7 +13,7 @@ onlineMusicQuizApp.factory('Quiz',
   // Get the number of questions in the quiz
   this.getNumberOfQuestions = function() {
     if ($cookies.get('numberOfQuestions') == undefined) {
-    this.setNumberOfGuests(5);
+    this.setNumberOfQuestions(5);
     return parseInt($cookies.get('numberOfQuestions'));
   }
   else {
@@ -49,62 +49,62 @@ onlineMusicQuizApp.factory('Quiz',
 //Borrowed =)
 //From here everything comes from http://jsfiddle.net/JMPerez/0u0v7e1b/
 // find template and compile it
-var templateSource = document.getElementById('results-template').innerHTML;
-var template = Handlebars.compile(templateSource);
-var resultsPlaceholder = document.getElementById('results');
-var playingCssClass = 'playing';
-var audioObject = null;
-
-var fetchTracks = function (albumId, callback) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            callback(response);
-        }
-    });
-};
-
-var searchAlbums = function (query) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: query,
-            type: 'album'
-        },
-        success: function (response) {
-            resultsPlaceholder.innerHTML = template(response);
-        }
-    });
-};
-
-results.addEventListener('click', function (e) {
-    var target = e.target;
-    if (target !== null && target.classList.contains('cover')) {
-        if (target.classList.contains(playingCssClass)) {
-            audioObject.pause();
-        } else {
-            if (audioObject) {
-                audioObject.pause();
-            }
-            fetchTracks(target.getAttribute('data-album-id'), function (data) {
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();
-                target.classList.add(playingCssClass);
-                audioObject.addEventListener('ended', function () {
-                    target.classList.remove(playingCssClass);
-                });
-                audioObject.addEventListener('pause', function () {
-                    target.classList.remove(playingCssClass);
-                });
-            });
-        }
-    }
-});
-
-document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    searchAlbums(document.getElementById('query').value);
-}, false);
+// var templateSource = document.getElementById('results-template').innerHTML;
+// var template = Handlebars.compile(templateSource);
+// var resultsPlaceholder = document.getElementById('results');
+// var playingCssClass = 'playing';
+// var audioObject = null;
+//
+// var fetchTracks = function (albumId, callback) {
+//     $.ajax({
+//         url: 'https://api.spotify.com/v1/albums/' + albumId,
+//         success: function (response) {
+//             callback(response);
+//         }
+//     });
+// };
+//
+// var searchAlbums = function (query) {
+//     $.ajax({
+//         url: 'https://api.spotify.com/v1/search',
+//         data: {
+//             q: query,
+//             type: 'album'
+//         },
+//         success: function (response) {
+//             resultsPlaceholder.innerHTML = template(response);
+//         }
+//     });
+// };
+//
+// results.addEventListener('click', function (e) {
+//     var target = e.target;
+//     if (target !== null && target.classList.contains('cover')) {
+//         if (target.classList.contains(playingCssClass)) {
+//             audioObject.pause();
+//         } else {
+//             if (audioObject) {
+//                 audioObject.pause();
+//             }
+//             fetchTracks(target.getAttribute('data-album-id'), function (data) {
+//                 audioObject = new Audio(data.tracks.items[0].preview_url);
+//                 audioObject.play();
+//                 target.classList.add(playingCssClass);
+//                 audioObject.addEventListener('ended', function () {
+//                     target.classList.remove(playingCssClass);
+//                 });
+//                 audioObject.addEventListener('pause', function () {
+//                     target.classList.remove(playingCssClass);
+//                 });
+//             });
+//         }
+//     }
+// });
+//
+// document.getElementById('search-form').addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     searchAlbums(document.getElementById('query').value);
+// }, false);
 
 
 
