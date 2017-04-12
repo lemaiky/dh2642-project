60cefcb6dd2e65fa5025708eeae7942ca3b0ab42
Mai-Khanh LE <lemaikhanh.lmk@gmail.com>
2017-04-12 16:05:29 +0200

    add artist from detail view

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,9 +1,5 @@
 onlineMusicQuizApp.controller('SearchCtrl', ['$scope','Quiz', function($scope,Quiz) {
 
-  $scope.addAlbum = function(album) {
-    Quiz.addAlbumToQuiz(album);
-  }
-
   // including the case while the search is still running.
   $scope.search = function(query,type) {
     Quiz.MusicSearch.get({q:query, type:'artist'},function(data){
