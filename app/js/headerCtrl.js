f7d0860540d219be2d03b594bb9199c3c44fa626
Lê Mai-Khanh <lemaikhanh.lmk@gmail.com>
2017-04-17 00:16:45 +0200

    quiz saved in profile

--- app/js/headerCtrl.js
+++ app/js/headerCtrl.js
@@ -1,5 +1,18 @@
-onlineMusicQuizApp.controller('HeaderCtrl', function($scope,Quiz) {
+onlineMusicQuizApp.controller('HeaderCtrl', function($scope,$firebaseAuth,Quiz) {
+
+  var auth = $firebaseAuth();
   $scope.signIn = function() {
-      Quiz.signIn();
+    if(Quiz.getUid() === "") {
+      // login with Google
+      auth.$signInWithPopup("google").then(function(firebaseUser) {
+        $scope.uid = firebaseUser.user.uid;
+        $scope.username = firebaseUser.user.displayName;
+        Quiz.setUid($scope.uid);
+        Quiz.setUsername($scope.username);
+        console.log("Signed in as:", firebaseUser.user);
+      }).catch(function(error) {
+        console.log("Authentication failed:", error);
+      });
+    }
   }
 });
--- app/js/resultsCtrl.js
+++ app/js/resultsCtrl.js
@@ -1,9 +1,37 @@
-onlineMusicQuizApp.controller('ResultsCtrl', function($scope,Quiz) {
+onlineMusicQuizApp.controller('ResultsCtrl', function($scope,$firebaseArray,Quiz) {
   $scope.score = Quiz.getNumberOfCorrectAnswers()
   $scope.numberOfQuestions = Quiz.getNumberOfQuestions();
   $scope.listOfAnswers = Quiz.getListOfAnswers();
 
   $scope.resetAnswers = function() {
     Quiz.resetAnswers();
+  }
+
+  $scope.saveQuiz = function() {
+    var uid = Quiz.getUid();
+    var username = Quiz.getUsername();
+    var artists = Quiz.getListArtistsId();
+    var quiz = Quiz.getQuestionList();
+    var score = Quiz.getNumberOfCorrectAnswers();
+
+    var newQuizKey = firebase.database().ref().child('quiz').push().key;
+
+    var userRef = $firebaseArray(firebase.database().ref('users/' + uid + '/' + newQuizKey));
+    userRef.$add({
+      author: username,
+      uid: uid,
+      artists: artists,
+      quiz: quiz,
+      score: score
+    })
+
+    var quizRef = $firebaseArray(firebase.database().ref('quiz/' + newQuizKey));
+    quizRef.$add({
+      author: username,
+      uid: uid,
+      artists: artists,
+      quiz: quiz,
+      score: score
+    })
   }
 });
