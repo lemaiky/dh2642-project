674c25cc75fa293144a37da28cf39234e65f909d
Mai-Khanh LE <lemaikhanh.lmk@gmail.com>
2017-04-19 21:34:22 +0200

    edit quiz multiple artists

--- app/js/sidebarCtrl.js
+++ app/js/sidebarCtrl.js
@@ -18,11 +18,11 @@ onlineMusicQuizApp.controller('SidebarCtrl', function($scope,$location,Quiz) {
 
       if(!contains) {
         Quiz.addAlbumToQuiz($scope.list2[i], $scope.list2[i].type);
-        Quiz.GetArtistAlbums.get({id:$scope.list2[i].id}, function(albums) {
-          for(var i in albums.items) {
-            Quiz.saveAlbumForQuiz(albums.items[i].id);
-          }
-        });
+        // Quiz.GetArtistAlbums.get({id:$scope.list2[i].id}, function(albums) {
+        //   for(var i in albums.items) {
+        //     Quiz.saveAlbumForQuiz(albums.items[i].id);
+        //   }
+        // });
       }
     }
     $scope.listView = Quiz.getChosenQuizMusic();
@@ -36,7 +36,7 @@ onlineMusicQuizApp.controller('SidebarCtrl', function($scope,$location,Quiz) {
         for(var j in albums.items) {
           Quiz.saveAlbumForQuiz(albums.items[j].id);
         }
-  
+
         if(index == $scope.listView.length - 1) {
           $location.path('quiz');
         } else {
